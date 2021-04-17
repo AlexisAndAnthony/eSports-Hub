bab37138bb768d3ca9191f872a6d2d679c5682c1
Alexis Glawitsch <lexiglawitsch@gmail.com>
2021-04-17 13:19:45 -0700

    Added color change to nav bar based on current page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,47 +1,84 @@
 import '../styles/App.css';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header(props) {
-    return (
-      <header className="header">
-        <div className="title-group">
-          <Link to="/" style={{ textDecoration: 'none' }}><h1>eSports Hub</h1></Link>
-          <p>Your one-stop-shop for eSports connections.</p>
-        </div>
-        <div className="nav-bar">
-          <NavBar isSignedIn={props.isSignedIn}/>
-        </div>
-      </header>
-    )
-  }
+  const location = useLocation();
+  console.log(location);
+  console.log(location['pathname']);
 
-  function NavBar(props) {
-    return (
+  return (
+    <header className="header">
+      <div className="title-group">
+        <Link to="/" style={{ textDecoration: 'none' }}><h1>eSports Hub</h1></Link>
+        <p>Your one-stop-shop for eSports connections.</p>
+      </div>
       <div className="nav-bar">
-        <div className="nav-links">
-          <Link to="/feed" style={{ textDecoration: 'none' }}><p>FEED</p></Link>
-          <Link to="/portfolios" style={{ textDecoration: 'none' }}><p>PORTFOLIOS</p></Link>
-          <Link to="/recruitment" style={{ textDecoration: 'none' }}><p>ESPORTS RECRUITMENT</p></Link>
-          <Link to="/groups" style={{ textDecoration: 'none' }}><p>GROUPS & SCRIMS</p></Link>
-          <Link to="/tournaments" style={{ textDecoration: 'none' }}><p>TOURNAMENTS</p></Link>
-        </div>
-        {!props.isSignedIn
-          ? <SignInButton />
-          : <p></p>
-        }
-      </div> 
-    )
-  }
-  
-  function SignInButton() {
-    return (
-      <Link to="/signup">
-        <button className="sign-in-button">
-            SIGN UP
-        </button>
-      </Link>
-    )
-  }
-  
-  export default Header;
+        <NavBar isSignedIn={props.isSignedIn} selectedLink={location.pathname}/>
+      </div>
+    </header>
+  )
+}
+
+function NavBar(props) {
+  console.log(props.selectedLink);
+  console.log(props.selectedLink == "/feed");
+
+  return (
+    <div className="nav-bar">
+      <div className="nav-links">
+        <Link 
+          to="/feed" 
+          style={{ textDecoration: 'none' }}
+          id={`${props.selectedLink == "/feed" ? "selected-link" : ""}`}
+        >
+          <p>FEED</p>
+        </Link>
+        <Link 
+          to="/portfolios" 
+          style={{ textDecoration: 'none' }}
+          id={props.selectedLink == "/portfolios" ? "selected-link" : ""}
+        >
+          <p>PORTFOLIOS</p>
+        </Link>
+        <Link 
+          to="/recruitment" 
+          style={{ textDecoration: 'none' }}
+          id={props.selectedLink == "/recruitment" ? "selected-link" : ""}
+        >
+          <p>ESPORTS RECRUITMENT</p>
+        </Link>
+        <Link 
+          to="/groups" 
+          style={{ textDecoration: 'none' }}
+          id={props.selectedLink == "/groups" ? "selected-link" : ""}
+        >
+          <p>GROUPS & SCRIMS</p>
+        </Link>
+        <Link 
+          to="/tournaments" 
+          style={{ textDecoration: 'none' }}
+          id={props.selectedLink == "/tournaments" ? "selected-link" : ""}
+        >
+          <p>TOURNAMENTS</p>
+        </Link>
+      </div>
+      {!props.isSignedIn
+        ? <SignInButton />
+        : <p></p>
+      }
+    </div> 
+  )
+}
+
+function SignInButton() {
+  return (
+    <Link to="/signup">
+      <button className="sign-in-button">
+          SIGN UP
+      </button>
+    </Link>
+  )
+}
+
+export default Header;
   
