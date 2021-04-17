096a2e78a737d8e3373dc11794c6aaf0fc2fdadf
AnthonySkoury <antjks99@gmail.com>
2021-04-16 20:56:53 -0700

    Updated serverjs

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
+// Import npm packages
 const express = require('express');
-const app = express();
-
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // promiseLibrary: global.Promise
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080; // Step 1
+
+const routes = require('./routes/api');
+
+// Step 2
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
+
+// Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Step 3
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 }
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+
+// HTTP request logger
+app.use(morgan('tiny'));
+app.use('/api', routes);
 
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
+
