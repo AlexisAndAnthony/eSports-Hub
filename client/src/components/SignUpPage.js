d4fe46cbd4698bc17e5c5b3e26ac4c165236e820
AnthonySkoury <antjks99@gmail.com>
2021-04-17 10:05:54 -0700

    Added google oauth

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,9 +1,14 @@
 import '../styles/App.css';
 import '../styles/SignIn.css';
 import Header from './Header.js';
-import { Link } from 'react-router-dom';
+import { GoogleLogin } from 'react-google-login';
+import * as data from '../config/default';
+const OAuthClientID = data['OAuthClientID'];
 
 function SignUpPage(props) {
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
   return (
     <div className="App">
       <Header isSignedIn={props.isSignedIn} />
@@ -11,12 +16,13 @@ function SignUpPage(props) {
         <h3>New to eSports Hub?</h3>
         <p>You can create an account in just a few quick steps.</p>
         <div>
-        </div>
-        <div className="sign-in-prompt">
-          <p>Already have an account?</p>
-          <Link to="/signin" style={{ textDecoration: 'none' }} >
-            <p>Click here to sign in.</p>
-          </Link>
+          <GoogleLogin
+            clientId={OAuthClientID}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
         </div>
       </div>
     </div>
